d16cdcfdffeff61f4b41d0e206405d49aa8dc00c
Daniel Martinez <martinezdaniel544@gmail.com>
2016-10-13 21:48:38 +0000

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Added as per devise installation instructions.
+  config.action_mailer.default_url_options = { host: 'danielsapp1988.herokuapp.com' }
 end
